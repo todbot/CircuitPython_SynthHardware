fb99d6454094a901b6ccfeeae4f75d8ecadf8974
Tod Kurt <todbot@users.noreply.github.com>
2023-10-11 12:38:14 -0700

    first

---
+++ synthhardware/picotouch_synth.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 Tod Kurt
+#
+# SPDX-License-Identifier: MIT
+
+
+
+def test_func(data, packet_size):
+    """Parse Test data
+    :param bytearray data: a data buffer containing a binary OSC packet
+    :param int packet_size: the size of the OSC packet (may be smaller than len(data))
+    """
+    print("hello", data, packet_size)
